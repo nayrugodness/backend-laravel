6072c251280bbde46ffcd92415355144c4d8baca
Nayru <nanualexandraramirez@gmail.com>
2022-06-06 21:45:22 -0500

    habilitando gd library

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,19 +35,18 @@
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Hoisu') }}
                 </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                <!--<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
+                    -- Left Side Of Navbar --
                     <ul class="navbar-nav mr-auto">
-
                     </ul>
 
-                    <!-- Right Side Of Navbar -->
+                    -- Right Side Of Navbar --
                     <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
+                        -- Authentication Links --
                         @guest
                             <li class="nav-item">
                                 <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
@@ -90,7 +89,7 @@
                             </li>
                         @endguest
                     </ul>
-                </div>
+                </div>-->
             </div>
         </nav>
 
