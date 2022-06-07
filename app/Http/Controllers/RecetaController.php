e56c51a4678f6fc5fb3bbabb5389141de88a8584
Nayru <nanualexandraramirez@gmail.com>
2022-06-07 03:41:54 -0500

    solving update

--- app/Http/Controllers/RecetaController.php
+++ app/Http/Controllers/RecetaController.php
@@ -200,8 +200,8 @@ class RecetaController extends Controller
             $ruta_imagen = $request['imagen']->store('upload-recetas', 'public');
 
             // Resize de la imagen
-            $img = Image::make( public_path("storage/{$ruta_imagen}"))->fit(1000, 550);
-            $img->save();
+            //$img = Image::make( public_path("storage/{$ruta_imagen}"))->fit(1000, 550);
+            //$img->save();
 
             // Asignar al objeto
             $receta->imagen = $ruta_imagen;
