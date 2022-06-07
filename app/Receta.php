9aaa9c3ca529dd367ac25fedf1d9a97d09e9d25f
Nayru <nanualexandraramirez@gmail.com>
2022-06-07 17:06:04 -0500

    adding fields to fillable in model

--- app/Receta.php
+++ app/Receta.php
@@ -9,7 +9,16 @@ class Receta extends Model
 
     // Campos que se agregaran
     protected $fillable = [
-        'titulo', 'descripcion', 'direccion', 'email', 'telefono',  'imagen', 'categoria_id', 'ciudad_id'
+        'titulo',
+        'descripcion', 
+        'direccion', 
+        'email', 
+        'telefono',  
+        'imagen', 
+        'apertura',
+        'cierre',
+        'categoria_id', 
+        'ciudad_id'
     ];
     
     // Obtiene la categoria de la receta via FK
