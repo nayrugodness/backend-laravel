a8d00b60dfa9bdc92acf9d9d351ea84e381625e7
Nayru <nanualexandraramirez@gmail.com>
2022-06-07 01:18:25 -0500

    adding cloudder service

--- config/app.php
+++ config/app.php
@@ -175,6 +175,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         App\Providers\CategoriasProvider::class,
+        JD\Cloudder\CloudderServiceProvider::class,
+
 
     ],
 
