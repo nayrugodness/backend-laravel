079afbf32853f96b11faccc7c21bc26e504600f4
Nayru <nanualexandraramirez@gmail.com>
2022-06-07 18:53:32 -0500

    adding style

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,6 +27,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/nav.css' ) }}" rel="stylesheet">
 </head>
 <body>
     <div id="app">
@@ -143,14 +144,14 @@
                 </nav>
             </div>
         </section>
-        <nav class="navbar navbar-expand-md navbar-light categorias-bg">
+        <!--<nav class="navbar navbar-expand-md navbar-light categorias-bg">
             <div class="container">
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#categorias" aria-controls="categorias" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                     Categorias
                 </button>
                 <div class="collapse navbar-collapse " id="categorias">
-                    <!-- Left Side Of Navbar -->
+                    Left Side Of Navbar 
                     <ul class="navbar-nav w-100 d-flex justify-content-between">
                         @foreach ($categorias as $categoria)
                         <li class="nav-item">
@@ -162,7 +163,7 @@
                     </ul>
                 </div>
             </div>
-        </nav>
+        </nav>-->
 
         @yield('hero')
 
