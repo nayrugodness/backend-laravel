8264289a164f9533ed353061af58d16ee6bfb4e5
Nayru <nanualexandraramirez@gmail.com>
2022-06-09 10:02:41 -0500

    adding folder reserva

---
+++ app/Reserva.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Reserva extends Model
+{
+    // Campos que se agregaran
+    protected $fillable = [
+        'hora',
+        'fecha',
+        'user_id', 
+        'receta_id'
+    ];
+    
+    // Obtiene el usuario que reserva via FK
+    public function comensal()
+    {
+       return $this->belongsTo(User::class, 'user_id');
+    }
+    // Obtiene el restaurante via FK
+    public function receta()
+    {
+       return $this->belongsTo(Receta::class);
+    }
+
+    // Obtiene la información del usuario via FK
+    //public function autor()
+    //{
+    //    return $this->belongsTo(User::class, 'user_id'); // FK de esta tabla
+    //}
+
+}
