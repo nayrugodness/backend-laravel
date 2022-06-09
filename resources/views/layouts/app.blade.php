904201f979fcc69b07a437d84d3648260c19656a
Nayru <nanualexandraramirez@gmail.com>
2022-06-09 14:03:09 -0500

    adding logout form

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -147,6 +147,9 @@
                         <li>
                         {{ __('Logout') }}
                         </li>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            @csrf
+                        </form>
                     </a>
                 </ul>
                 @endguest
