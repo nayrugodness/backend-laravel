f7a425f6a19df29fec6f9d2f721d72ab57486176
Nayru <nanualexandraramirez@gmail.com>
2022-06-12 21:00:50 -0500

    no more btns unnecesary

--- resources/views/ui/navegacion.blade.php
+++ resources/views/ui/navegacion.blade.php
@@ -8,9 +8,9 @@
     <a href="{{ route('recetas.create') }}" class="btn btn-outline-primary mr-2 text-uppercase font-weight-bold" style="width: 20rem; margin-top:1rem;">
         Registrar restaurante
     </a>
-    <a href="{{ route('perfiles.edit', ['perfil' => Auth::user()->id ]) }}" class="btn btn-outline-success mr-2 text-uppercase font-weight-bold" style="width: 20rem; margin-top:1rem;">
+    <!--<a href="{{ route('perfiles.edit', ['perfil' => Auth::user()->id ]) }}" class="btn btn-outline-success mr-2 text-uppercase font-weight-bold" style="width: 20rem; margin-top:1rem;">
         Editar mi perfil</a>
     <a href="{{ route('perfiles.show', ['perfil' => Auth::user()->id ]) }}" class="btn btn-outline-info mr-2 text-uppercase font-weight-bold" style="width: 20rem; margin-top:1rem;">
         Ver mi perfil
-    </a>
+    </a>-->
 </div>
